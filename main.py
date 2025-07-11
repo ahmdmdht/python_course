3c4e2b84f2f672f759a830d002afa4feae87c857
ahmed medhat <89140116+ahmdmdht@users.noreply.github.com>
2025-07-11 17:01:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import email_validation
-import letter_location_with_index
+import email_validation as e_v
+import letter_location_with_index as lli
 import multiplication
-import sorted_elements
-import vowel_count
-import mario_pyramid
+import sorted_elements as s_e
+import vowel_count as v_c
+import mario_pyramid as m_p
 
 ## check name correcteness
 user_input = input("Enter your name: ")
-print(email_validation.name_validation(user_input))
+print(e_v.name_validation(user_input))
 
 
 ## define letter i locator and sum number of i letters
 word=input("enter the word : ")
-print(letter_location_with_index.i_location(word))
+print(lli.i_location(word))
 
 
 #### make mario pyramid in list and its height given by user
 numOfList=int(input("num of elements in list:  "))
 list=[]
-print(mario_pyramid.pyramid_in_list(numOfList))
+print(m_p.pyramid_in_list(numOfList))
 
 
 ### give the result of POWER OF ELEMENT given by the user
@@ -29,11 +29,11 @@ print(multiplication.power_of_nums(user_input))
 ### sort elements given by the user ascending or descending
 num_Of_elements=int(input("num of elements in list :  "))
 list=[]
-print(sorted_elements.sorted_elements(num_Of_elements))
+print(s_e.sorted_elements(num_Of_elements))
 
 
 ### count the vowel letters in word given by user
 y=input("enter the word : ")
-print(vowel_count.vowelcount(y))
+print(v_c.vowelcount(y))
 
 
