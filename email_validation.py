0804c1105d213c4277098e9e693f2e8673f4ef5f
ahmed medhat <89140116+ahmdmdht@users.noreply.github.com>
2025-07-11 16:45:23 +0300

    Update email_validation.py

--- email_validation.py
+++ email_validation.py
@@ -1,39 +1,39 @@
 ## a program that user enter number of emails then check if validated
-def validEmail(email):
- try:
-    if '@' in email and '.' in email:
-        username, domain = email.split('@')
+# def validEmail(email):
+#  try:
+#     if '@' in email and '.' in email:
+#         username, domain = email.split('@')
         
-        if username and domain:
-            if "." in domain:
-                x,*y=domain.split('.')
-                if x and y:
-                    return True
-                else : 
-                    return False
-            else :
-                return False
-        else : 
-            return False
-    else :
-        return False
- except:
-     return False
+#         if username and domain:
+#             if "." in domain:
+#                 x,*y=domain.split('.')
+#                 if x and y:
+#                     return True
+#                 else : 
+#                     return False
+#             else :
+#                 return False
+#         else : 
+#             return False
+#     else :
+#         return False
+#  except:
+#      return False
   
 
  
-num_of_emails = int(input("enter the number of emails: "))
-for i in range(1 ,num_of_emails+1):
-     email = input("Enter your email: ")
-     if validEmail(email):
-      print("valid email")
-      break
-     else:
-       print("unvalid email")
+# num_of_emails = int(input("enter the number of emails: "))
+# for i in range(1 ,num_of_emails+1):
+#      email = input("Enter your email: ")
+#      if validEmail(email):
+#       print("valid email")
+#       break
+#      else:
+#        print("unvalid email")
        
        
        
-print("*"*50)     
+# print("*"*50)     
 ########## a program that check if user enter name correctly without spaces or empty
 
 
@@ -49,24 +49,3 @@ def name_validation(x):
      print("Invalid input. Please use alphabetic characters only.")
      
 name_validation(user_input)
-
-
-### a program that check if the name and pass is correct using dictionary
-
-
-user = [{"name":"omar","pass":"123"},{"name":"ahmed","pass":"456"}]
-userInput=input("enter ur name:  ")
-for i in user:
-    if i["name"] ==userInput:
-        userPass=input("enter ur pass:  ")
-        if i["pass"] ==userPass:
-            print("ur pass correct")
-            break
-        else:
-            print("uncorrect password")
-        break
-    
-    else:
-        continue
-else:
-    print("not found")
